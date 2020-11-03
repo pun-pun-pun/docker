e3cf9ce694c8dfa192685f19fb4b765388c77097
pun-pun-pun <nnkhdak@yahoo.co.jp>
2020-11-04 00:39:06 +0900

    commit

--- php/html/index.php
+++ php/html/index.php
@@ -5,3 +5,16 @@ require_once("../php/import.php");
 echo "ccc\n";
 ?>
 aaa
+
+<?php
+require_once('../php/Statics.php');
+Statics::putEnvironment();
+
+echo "hogehoge[";
+echo getenv('hogehoge');
+echo "]\n";
+echo "environment[";
+echo getenv('environment');
+echo "]\n";
+?>
+aaa
